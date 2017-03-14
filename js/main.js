6de88e77d0aa1e541a3380bdc04d3c4a7f0a9301
hhalldorson <HillaryHalldorson@017-GDMA-Mac.local>
2017-03-13 19:02:03 -0700

    Class Session 2
    Added Styling, content and js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 
+$('#taptostart').click( function() {
+  $('#startSection').fadeOut(250);
+  $('#step1').fadeIn(250);
+});
+
+setTimeout(function() {
+  $('#startScreen').addClass('test');
+}, 5000)
+
+setTimeout(function() {
+  $('#startScreen').removeClass('test');
+}, 15000)
+
+$('.next-button').click( function(){
+  $(this).closest('.content-box').fadeOut(500);
+  $( $(this).attr('href') ).fadeIn(750);
+});
+
+$('.select-item a').click( function(){
+  $(this).closest('.group').find('.order-item').hide();
+  $( $(this).attr('href') ).show();
+});
+
+$('.btn-close').click(
+  function(){
+    $(this).closest('.content-box').fadeOut(500);
+  }
+)
